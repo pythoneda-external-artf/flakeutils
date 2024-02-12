17602749d9daa215b80cf84ca4f564ea09ce7096
rydnr <github@acm-sl.org>
2024-02-12 12:40:30 +0100

    Moved flake from pythoneda-shared-nix-flake

---
+++ pythoneda/artifact/external/flakeutils/flake_utils_nix_flake.py
@@ -0,0 +1,77 @@
+# vim: set fileencoding=utf-8
+"""
+pythoneda/shared/nix/flake/flake_utils_nix_flake.py
+
+This file defines the FlakeUtilsNixFlake class.
+
+Copyright (C) 2024-today rydnr's pythoneda-external-artf/flakeutils
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from pythoneda.shared.nix.flake import NixFlake
+
+
+class FlakeUtilsNixFlake(NixFlake):
+
+    """
+    Represents a flake-utils Nix flake.
+
+    Class name: FlakeUtilsNixFlake
+
+    Responsibilities:
+        - Provides a way to build numtide/flake-utils.
+        - Provides a way to run numtide/flake-utils.
+
+    Collaborators:
+        - pythoneda.shared.nix.flake.NixFlake
+    """
+
+    def __init__(self, version: str):
+        """
+        Creates a new FlakeUtilsNixFlake instance.
+        :param version: The version.
+        :type version: str
+        """
+        super().__init__(
+            "flake-utils",
+            version,
+            "github:numtide/flake-utils/{version}",
+            [],
+            None,
+            "Pure Nix flake utility functions",
+            "https://github.com/numtide/flake-utils",
+            "mit",
+            list("https://github.com/zimbatm"),
+            2022,
+            "https://github.com/numtide",
+        )
+
+    @classmethod
+    def default(cls):
+        """
+        Retrieves the default version of the flake-utils Nix flake input.
+        :return: Such instance.
+        :rtype: pythoneda.shared.nix.flake.FlakeUtilsNixFlake
+        """
+        return cls("v1.0.0")
+
+
+# vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
+# Local Variables:
+# mode: python
+# python-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# fill-column: 79
+# End:
